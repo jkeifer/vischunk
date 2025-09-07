9e1015a6f2b467a2bbb2530992c9f2da44c29897
Jarrett Keifer <jkeifer0@gmail.com>
2025-09-06 19:58:22 -0700

    who set these coverage thresholds?

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 export default {
-  testEnvironment: 'node',
-  transform: {},
-  setupFilesAfterEnv: ['jest-canvas-mock'],
-  collectCoverageFrom: [
-    'src/js/**/*.js',
-    '!src/js/main.js', // Entry point
-    '!src/js/visualization/canvas-manager.js' // DOM-dependent
-  ],
-  coverageThreshold: {
+    testEnvironment: 'node',
+    transform: {},
+    setupFilesAfterEnv: ['jest-canvas-mock'],
+    collectCoverageFrom: [
+        'src/js/**/*.js',
+        '!src/js/main.js', // Entry point
+        '!src/js/visualization/canvas-manager.js', // DOM-dependent
+    ],
+    /*coverageThreshold: {
     global: {
       branches: 70,
       functions: 70,
@@ -26,5 +26,5 @@ export default {
       lines: 75,
       statements: 75
     }
-  }
+  }*/
 };
